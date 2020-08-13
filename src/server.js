aa12f3588c018e3ef1802dc3df052c4825fc2286
Lip Cowan <pcowan90@gmail.com>
2020-08-13 09:20:05 -0700

    boilerplate completed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
+const {PORT} = require('./config')
 
-const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
